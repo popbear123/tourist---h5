63fd42c1e8cb3858bb8b8a4a4d20080ffd60487e
popbear123 <1132158392@qq.com>
2023-04-07 09:32:40 +0800

    初始化项目

---
+++ src/assets/data/tabbar.ts
@@ -0,0 +1,26 @@
+export const tabbarList = [
+  {
+    text: '首页',
+    imageURL: 'img/tabbar/tab_home.png',
+    activeImageURL: 'img/tabbar/tab_home_active.png',
+    path: '/home'
+  },
+  {
+    text: '收藏',
+    imageURL: 'img/tabbar/tab_favor.png',
+    activeImageURL: 'img/tabbar/tab_favor_active.png',
+    path: '/favor'
+  },
+  {
+    text: '订单',
+    imageURL: 'img/tabbar/tab_order.png',
+    activeImageURL: 'img/tabbar/tab_order_active.png',
+    path: '/order'
+  },
+  {
+    text: '消息',
+    imageURL: 'img/tabbar/tab_message.png',
+    activeImageURL: 'img/tabbar/tab_message_active.png',
+    path: '/message'
+  }
+]
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from '@/router'
+import store from '@/store'
+
+import 'normalize.css'
+import '@/assets/css/index.scss'
+
+createApp(App).use(router).use(store).mount('#app')
